10adc10468adb81f79b0e5957bb6c49d3729f1af
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2024-04-04 23:10:26 -0400

    Make the layer more easy to use
    - Place the input size at the begining in the constructor
    - Return a single neuron if the outputs contains only one element

--- layer.py
+++ layer.py
@@ -2,8 +2,9 @@ from typing import List
 from neuron import Neuron
 
 class Layer:
-  def __init__(self, num_neurons: int, input_size: int):
+  def __init__(self, input_size: int, num_neurons: int):
     self.neurons = [Neuron(input_size) for _ in range(num_neurons)]
   
   def __call__(self, input_values: List[float]):
-    return [n(input_values) for n in self.neurons]
+    outputs = [n(input_values) for n in self.neurons]
+    return outputs[0] if len(outputs) == 1 else outputs
